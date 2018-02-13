3c4c868239cfa45e2420ddeb2fa46cda9eaa6ff5
hzxcaryn <heng.zhixuan.caryn@dhs.sg>
2018-02-14 01:51:07 +0800

    Add Contact class with common methods extracted

---
+++ src/seedu/addressbook/data/person/Contact.java
@@ -0,0 +1,44 @@
+package seedu.addressbook.data.person;
+
+import seedu.addressbook.data.exception.IllegalValueException;
+
+public class Contact {
+
+    public final String value;
+    private boolean isPrivate;
+
+    /**
+     * Validates given phone number.
+     *
+     * @throws IllegalValueException if given phone string is invalid.
+     */
+    public Contact(String data, boolean isPrivate, String validationRegex, String constraints) throws IllegalValueException {
+        this.isPrivate = isPrivate;
+        String trimmedData = data.trim();
+        if (!isValid(trimmedData, validationRegex)) {
+            throw new IllegalValueException(constraints);
+        }
+        this.value = trimmedData;
+    }
+
+    /**
+     * Returns true if the given string is a valid person phone number.
+     */
+    public static boolean isValid(String test, String validationRegex) {
+        return test.matches(validationRegex);
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+
+    public boolean isPrivate() {
+        return isPrivate;
+    }
+}
